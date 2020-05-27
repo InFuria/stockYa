2e56f3fbf64f2170823de23d9cbf7b4cb5c876e8
vagrant <vagrant@homestead>
2020-05-27 03:36:18 +0000

    config files changes

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return [
     |
     */
 
-    'fallback_locale' => 'en',
+    'fallback_locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
@@ -106,7 +106,7 @@ return [
     |
     */
 
-    'faker_locale' => 'en_US',
+    'faker_locale' => 'es_ES',
 
     /*
     |--------------------------------------------------------------------------
@@ -161,8 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
-        UxWeb\SweetAlert\SweetAlertServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -227,9 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
-        'Alert' => UxWeb\SweetAlert\SweetAlert::class,
-
     ],
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,38 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('create', function (){
+
+    dd(csrf_token());
+    $product = \App\Product::where('id', 101)->first();
+    return view('create', compact('product'));
 });
+
+/** PASSPORT **/
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
+
+/** PRODUCTS ROUTES  **/
+Route::get('products/index', 'Api\ProductController@index');
+
+Route::get('products', 'Api\ProductController@getProducts')->name('products.getProducts');
+Route::post('products', 'Api\ProductController@store')->name('products.store');
+Route::get('products/select/{product}', 'Api\ProductController@select')->name('products.select');
+Route::put('products/{product}', 'Api\ProductController@update')->name('products.update');
+Route::patch('products/{product}', 'Api\ProductController@update')->name('products.update');
+Route::delete('products/{product}', 'Api\ProductController@destroy')->name('products.destroy');
+Route::post('products/{product}/status', 'Api\ProductController@status')->name('products.status');
+Route::post('products/{product}/setScore', 'Api\ProductController@setScore')->name('products.setScore');
+
+
+/** COMPANIES ROUTES **/
+Route::get('companies', 'Api\CompanyController@getCompanies')->name('companies.getCompanies');
+Route::get('categories', 'Api\CompanyController@listCategories')->name('categories.listCategories');
+
