3fdb7d2950ee4dcda7267d16218239e05ff34b55
vagrant <vagrant@homestead>
2020-06-07 12:26:13 -0400

    make unique slugs to entities

--- database/migrations/2020_05_17_153314_create_products_table.php
+++ database/migrations/2020_05_17_153314_create_products_table.php
@@ -27,7 +27,7 @@ class CreateProductsTable extends Migration
 
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->string('name');
             $table->string('description');
             $table->string('type');
